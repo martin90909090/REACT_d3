7a492ba358edd4ddd65b6f13b2894e30710b18b6
Martín Pérez Ruiz <personalmperez@gmail.com>
2024-02-20 20:30:55 -0300

    m

--- REACT_d3/src/App.jsx
+++ REACT_d3/src/App.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from 'react'
 import './App.scss'
-import Alert from './components/Alert'
-import Buscador from './components/Buscador'
-import Formulario from './components/Formulario'
-import Listado from './components/Listado'
-import { BaseColaboradores } from './assets/script'
+import Alert from './Components/Alert/Alert.jsx'
+import Buscador from './Components/Buscador/Buscador.jsx'
+import Formulario from './Components/Formulario/Formulario.jsx'
+import Listado from './Components/Listado/Listado.jsx'
+import { BaseColaboradores } from './assets/JS/script.js'
 
 const App = () => {
 
   return (
     <>
-      
+      <Listado />
     </>
   )
 }
 
-export default App
+export default App; 
--- REACT_d3/src/Components/Listado/Listado.jsx
+++ REACT_d3/src/Components/Listado/Listado.jsx
@@ -1,8 +1,32 @@
-import { BaseColaboradores } from "../../assets/script";
+import { BaseColaboradores } from "../../assets/JS/script.js";
+import {Table} from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 const Listado = () => {
-
+    return (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Edad</th>
+              <th>Cargo</th>
+              <th>Teléfono</th>
+            </tr>
+          </thead>
+          <tbody>
+            {BaseColaboradores.map((colaborador, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{colaborador.nombre}</td>
+                <td>{colaborador.email}</td>
+                <td>{colaborador.posicion}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      );
 };
 
 export default Listado;
