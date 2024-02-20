f77de93ae77ee32888f8333e2cf91346afdb12d5
Martín Pérez Ruiz <personalmperez@gmail.com>
2024-02-20 20:16:53 -0300

    m

--- REACT_d3/src/App.jsx
+++ REACT_d3/src/App.jsx
@@ -4,8 +4,9 @@ import Alert from './components/Alert'
 import Buscador from './components/Buscador'
 import Formulario from './components/Formulario'
 import Listado from './components/Listado'
+import { BaseColaboradores } from './assets/script'
 
-function App() {
+const App = () => {
 
   return (
     <>
---
+++ REACT_d3/src/Components/Listado/Listado.jsx
@@ -0,0 +1,8 @@
+import { BaseColaboradores } from "../../assets/script";
+import { useState, useEffect } from "react";
+
+const Listado = () => {
+
+};
+
+export default Listado;
