53631bb0907523416b38a041283db1e3c7e17819
Martín Pérez Ruiz <personalmperez@gmail.com>
2024-02-23 21:11:51 -0300

    m

--- REACT_d3/src/App.jsx
+++ REACT_d3/src/App.jsx
@@ -1,13 +1,23 @@
 import BaseColaboradores from './assets/JS/script.js';
 import Listado from './Components/Listado/Listado.jsx';
+import { useState } from 'react';
 
 const App = () => {
+  const [mostrarListado, setMostrarListado] = useState(true);
+
+  const toggleListado = () => {
+      setMostrarListado(!mostrarListado);
+  };
+  
   return (
     <div className="App">
       <h1>Lista de Colaboradores</h1>
-      <Listado colaboradores={ BaseColaboradores } />
+      <button onClick={toggleListado}>
+                {mostrarListado ? 'Ocultar Listado' : 'Mostrar Listado'}
+      </button>
+      {mostrarListado && <Listado colaboradores={ BaseColaboradores } />}
     </div>
-  );
+  )
 };
 
 export default App;
